60184784b3375768d7a0f9d63d683f805c3ae213
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-11 22:41:40 +0100

    Add files via upload

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  var myFirstName = "Kerim";
+  var myLastName = "Imamovic";
+  var myHomeTown = "Sarajevo";
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+
+module.exports = main;
